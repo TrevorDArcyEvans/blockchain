54fb9eee5c4e3cf0808c9ce553798fed93278753
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-12-05 21:41:54 +0000

    - code cleanup

--- csharp/BlockChain/Block.cs
+++ csharp/BlockChain/Block.cs
@@ -13,7 +13,7 @@
 
     public override string ToString()
     {
-      return $"{Index} [{Timestamp.ToString("yyyy-MM-dd HH:mm:ss")}] Proof: {Proof} | PrevHash: {PreviousHash} | Trx: {Transactions.Count}";
+      return $"{Index} [{Timestamp:yyyy-MM-dd HH:mm:ss}] Proof: {Proof} | PrevHash: {PreviousHash} | Trx: {Transactions.Count}";
     }
   }
 }
